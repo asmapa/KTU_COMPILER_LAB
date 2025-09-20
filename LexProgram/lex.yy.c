f5c1dbc873aaa149e8c5666770744665685d23ba
asmapa <asmapa122002@gmail.com>
2025-09-20 15:30:18 +0530

    updated

--- LexProgram/lex.yy.c
+++ LexProgram/lex.yy.c
@@ -363,9 +363,9 @@ struct yy_trans_info
 	flex_int32_t yy_verify;
 	flex_int32_t yy_nxt;
 	};
-static const flex_int16_t yy_accept[7] =
+static const flex_int16_t yy_accept[8] =
     {   0,
-        0,    0,    4,    2,    1,    0
+        0,    0,    4,    2,    3,    1,    0
     } ;
 
 static const YY_CHAR yy_ec[256] =
@@ -405,24 +405,24 @@ static const YY_CHAR yy_meta[4] =
         1,    1,    1
     } ;
 
-static const flex_int16_t yy_base[7] =
+static const flex_int16_t yy_base[8] =
     {   0,
-        0,    0,    4,    5,    5,    5
+        0,    0,    4,    5,    5,    5,    5
     } ;
 
-static const flex_int16_t yy_def[7] =
+static const flex_int16_t yy_def[8] =
     {   0,
-        6,    1,    6,    6,    6,    0
+        7,    1,    7,    7,    7,    7,    0
     } ;
 
 static const flex_int16_t yy_nxt[9] =
     {   0,
-        4,    4,    5,    6,    3,    6,    6,    6
+        4,    5,    6,    7,    3,    7,    7,    7
     } ;
 
 static const flex_int16_t yy_chk[9] =
     {   0,
-        1,    1,    1,    3,    6,    6,    6,    6
+        1,    1,    1,    3,    7,    7,    7,    7
     } ;
 
 static yy_state_type yy_last_accepting_state;
@@ -439,11 +439,12 @@ int yy_flex_debug = 0;
 #define YY_MORE_ADJ 0
 #define YY_RESTORE_YY_MORE_OFFSET
 char *yytext;
-#line 1 "upper_to_lower.l"
-#line 2 "upper_to_lower.l"
+#line 1 "all_toupper.l"
+#line 2 "all_toupper.l"
     #include<stdio.h>
-#line 446 "lex.yy.c"
+    #include<ctype.h>
 #line 447 "lex.yy.c"
+#line 448 "lex.yy.c"
 
 #define INITIAL 0
 
@@ -660,9 +661,9 @@ YY_DECL
 		}
 
 	{
-#line 5 "upper_to_lower.l"
+#line 8 "all_toupper.l"
 
-#line 666 "lex.yy.c"
+#line 667 "lex.yy.c"
 
 	while ( /*CONSTCOND*/1 )		/* loops until end-of-file is reached */
 		{
@@ -689,7 +690,7 @@ yy_match:
 			while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 				{
 				yy_current_state = (int) yy_def[yy_current_state];
-				if ( yy_current_state >= 7 )
+				if ( yy_current_state >= 8 )
 					yy_c = yy_meta[yy_c];
 				}
 			yy_current_state = yy_nxt[yy_base[yy_current_state] + yy_c];
@@ -721,18 +722,17 @@ do_action:	/* This label is used only to access EOF actions. */
 
 case 1:
 YY_RULE_SETUP
-#line 6 "upper_to_lower.l"
-{printf("%c",yytext[0]-32);}
+#line 9 "all_toupper.l"
+{putchar(toupper(yytext[0]));}
 	YY_BREAK
 case 2:
-/* rule 2 can match eol */
 YY_RULE_SETUP
-#line 7 "upper_to_lower.l"
-{printf("%s",yytext);}
+#line 10 "all_toupper.l"
+{putchar(yytext[0]);}
 	YY_BREAK
 case 3:
 YY_RULE_SETUP
-#line 8 "upper_to_lower.l"
+#line 11 "all_toupper.l"
 ECHO;
 	YY_BREAK
 #line 739 "lex.yy.c"
@@ -1032,7 +1032,7 @@ static int yy_get_next_buffer (void)
 		while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 			{
 			yy_current_state = (int) yy_def[yy_current_state];
-			if ( yy_current_state >= 7 )
+			if ( yy_current_state >= 8 )
 				yy_c = yy_meta[yy_c];
 			}
 		yy_current_state = yy_nxt[yy_base[yy_current_state] + yy_c];
@@ -1060,11 +1060,11 @@ static int yy_get_next_buffer (void)
 	while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 		{
 		yy_current_state = (int) yy_def[yy_current_state];
-		if ( yy_current_state >= 7 )
+		if ( yy_current_state >= 8 )
 			yy_c = yy_meta[yy_c];
 		}
 	yy_current_state = yy_nxt[yy_base[yy_current_state] + yy_c];
-	yy_is_jam = (yy_current_state == 6);
+	yy_is_jam = (yy_current_state == 7);
 
 		return yy_is_jam ? 0 : yy_current_state;
 }
@@ -1740,10 +1740,10 @@ void yyfree (void * ptr )
 
 #define YYTABLES_NAME "yytables"
 
-#line 8 "upper_to_lower.l"
+#line 11 "all_toupper.l"
 
 
 int main(){
-    printf("converted string:");
+    printf("enter string:");
     yylex();
 }
